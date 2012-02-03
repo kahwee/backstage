d13e57c290ec374e775976edd4a5c64b2978e8c1
KahWee Teng <t@kw.sg>
2012-02-03 17:53:11 +0800

    Discover models and their classes.

--- protected/modules/backstage/controllers/DefaultController.php
+++ protected/modules/backstage/controllers/DefaultController.php
@@ -6,4 +6,23 @@ class DefaultController extends Controller
 	{
 		$this->render('index');
 	}
+
+	public function actionD() {
+		$model_names = $this->k_return_models();
+		print_r($model_names);
+	}
+
+	public function k_return_models() {
+		$models_path = Yii::getPathOfAlias('application.models');
+		$model_names = array();
+		$model_files = CFileHelper::findFiles($models_path, array(
+			'level' => 0,
+			'fileTypes' => array('php'),
+		));
+		foreach ($model_files as $model_file) {
+			$model_names[] = str_replace(DIRECTORY_SEPARATOR, '', str_replace('.php', '', str_replace($models_path, '', $model_file)));
+		}
+		return $model_names;
+	}
+
 }
